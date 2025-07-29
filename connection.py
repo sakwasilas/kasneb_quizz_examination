41b7addd6729d2fabec8f23760a387f592497262
sakwasilas <sakwahsilas@gmail.com>
2025-07-29 09:59:01 +0300

    made correcrion on yaml

--- connection.py
+++ connection.py
@@ -2,12 +2,9 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-
 DB_URL = os.environ.get("DATABASE_URL")
-
 if not DB_URL:
-    DB_URL = "postgresql://postgres:2480@localhost:5432/exams_25"
-
+    DB_URL = "postgresql+psycopg2://exams_25_user:DWhPyXT1eKVhPdkKmPC3XglqrJIVXTsZ@dpg-d246sh1r0fns73arnc6g-a:5432/exams_25"
 
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
