92547ae1032c69e4874ee7541daef4170479b956
sakwasilas <sakwahsilas@gmail.com>
2025-07-29 09:33:39 +0300

    updated connection.py

--- connection.py
+++ connection.py
@@ -1,16 +1,13 @@
+
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Get from environment variables (Render and local .env)
-DB_URL = os.environ.get("DATABASE_URL")  # Render will set this
 
-# Fallback for local development (optional)
-if not DB_URL:
-    username = "root"
-    password = "2480"
-    database = "exams_25"
-    DB_URL = f"mysql+mysqldb://{username}:{password}@localhost/{database}?charset=utf8mb4"
+DB_URL = os.environ.get("DATABASE_URL")
+
+
+DB_URL = "postgresql://postgres:yourpassword@localhost:5432/exams_25"
 
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
