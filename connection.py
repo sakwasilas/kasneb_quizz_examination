e89e379b729ddb4f655f4204c087f421bdbdf71e
sakwasilas <sakwahsilas@gmail.com>
2025-07-29 16:29:46 +0300

    gunicorn

--- connection.py
+++ connection.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session, declarative_base
 
-DATABASE_URL = "postgresql+psycopg2://exams_25_hax1:muCCD3uQohdUFIBe23co6fRZZJ26hE58@dpg-d24b0p8gjchc7388c5bg-a.oregon-postgres.render.com:5432/exams_25_hax1"
+DATABASE_URL = "postgresql+psycopg2://exams_25_6cm_user:<your-password>@dpg-d24clsidbo4c73ajf8mg-a.oregon-postgres.render.com:5432/exams_25_6cmx"
 
 engine = create_engine(DATABASE_URL)
 
