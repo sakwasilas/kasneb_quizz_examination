e8baad57e94eb808cf1e94c7316928126c9d9bff
sakwasilas <sakwahsilas@gmail.com>
2025-07-29 09:35:41 +0300

    updated connection.py

--- connection.py
+++ connection.py
@@ -1,4 +1,3 @@
-
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
@@ -6,8 +5,9 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DB_URL = os.environ.get("DATABASE_URL")
 
+if not DB_URL:
+    DB_URL = "postgresql://postgres:2480@localhost:5432/exams_25"
 
-DB_URL = "postgresql://postgres:yourpassword@localhost:5432/exams_25"
 
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
