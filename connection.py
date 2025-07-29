84306559a839a5a44075f17b0ee17474cc3b6f4c
sakwasilas <sakwahsilas@gmail.com>
2025-07-29 16:44:10 +0300

    modfied connection

--- connection.py
+++ connection.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session, declarative_base
 
-DATABASE_URL = "postgresql+psycopg2://exams_25_6cm_user:<your-password>@dpg-d24clsidbo4c73ajf8mg-a.oregon-postgres.render.com:5432/exams_25_6cmx"
+DATABASE_URL = "postgresql://exams_25_6cmx_user:RzZ0nHIZPHBVbWSTjgln9q2wLlEgVUE4@dpg-d24clsidbo4c73ajf8mg-a.oregon-postgres.render.com:5432/exams_25_6cmx?sslmode=require"
 
 engine = create_engine(DATABASE_URL)
 
